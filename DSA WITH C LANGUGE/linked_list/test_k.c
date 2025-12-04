7f1e39634c5e7a31da8aa153bc801b2387cd4d05
Emadsiddig <emadsiddig72@gmail.com>
2025-12-05 00:18:36 +0200

    Update test_k.c

--- DSA WITH C LANGUGE/linked_list/test_k.c
+++ DSA WITH C LANGUGE/linked_list/test_k.c
@@ -1,34 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+
+struct node{
+	int data;
+	struct node *next;
+};
 
 int main(){
-char name,fav1,fav2,fav3;
-int age,mark1,mark2,mark3,total,avarege;
-printf("enter your name:");
-scanf("%s",&name);
+	int x,n;
+	printf("how many nodes you want to add:");
+	scanf("%d",&n);
+	struct node *head ;
+	printf("enter your head element:");
+	scanf("%d",&x);
+	head->data =x;
+	head->next=0;
 
-printf("\nenter fav1:");
-scanf("%s",&fav1);
+	if{
 
-printf("\nenter fav2:");
-scanf("%s",&fav2);
-
-printf("\nenter fav3:");
-scanf("%s",&fav3);
-
-printf("enter the mark1:");
-scanf("%d",&mark1);
-
-printf("enter the mark2:");
-scanf("%d",&mark2);
-
-printf("enter the mark3:");
-scanf("%d",&mark3);
-total=mark1+mark2+mark3;
-avarege=total/3;
-printf("the total :%d and the averegegg :%d",total,avarege);
-
-
-
-	return 0;
+	}
 }
